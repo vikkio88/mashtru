92ab04f8ae81ad32221f3146fb4369df73bdd2dd
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2017-02-06 22:17:53 +0000

    fix job interface

--- src/Libs/Interfaces/Job.php
+++ src/Libs/Interfaces/Job.php
@@ -6,5 +6,14 @@ namespace Mashtru\Libs\Interfaces;
 
 interface Job
 {
+    /**
+     * @param array $parameters
+     * @return int
+     */
     public function fire(array $parameters = []);
+
+    /**
+     * @return array
+     */
+    public function toArray();
 }
