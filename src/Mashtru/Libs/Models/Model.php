4eef899f8afc0ccd61908f753dac3e1b5f57cf05
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2017-02-07 20:39:03 +0000

    added abstract db model

---
+++ src/Mashtru/Libs/Models/Model.php
@@ -0,0 +1,86 @@
+<?php
+
+
+namespace Mashtru\Libs\Models;
+
+
+use DateTime;
+use LessQL\Database;
+use PDO;
+
+abstract class Model
+{
+    protected $tableName;
+    protected $db;
+    protected $pdo;
+    private $config;
+
+    public function __construct(array $config, $table)
+    {
+        $this->config = $config;
+        $this->pdo = new PDO(
+            sprintf(
+                'mysql:host=%s;dbname=%s',
+                $this->config['host'],
+                $this->config['db']
+            ),
+            $this->config['user'],
+            $this->config['password']
+        );
+        $this->db = new Database($this->pdo);
+        $this->tableName = $table;
+    }
+
+    protected function table()
+    {
+        return $this->db->table($this->tableName);
+    }
+
+    public function getOne($id)
+    {
+        return $this->table()
+            ->where('id', $id)
+            ->fetch();
+    }
+
+    public function getAll()
+    {
+        return $this->table()->fetchAll();
+    }
+
+    public function create($data)
+    {
+        $data = array_merge(
+            $data,
+            ['created_at' => (new DateTime())->format('Y-m-d H:i:s')]
+        );
+
+        $row = $this->db->createRow(
+            $this->tableName,
+            $data
+        );
+
+        $this->db->begin();
+        $row->save();
+        return $this->db->commit();
+
+    }
+
+    public function update($id, $data)
+    {
+
+    }
+
+    public function delete($id)
+    {
+        return $this->table()
+            ->where('id', $id)
+            ->delete();
+    }
+
+    public function truncate()
+    {
+        $this->pdo->exec(sprintf('TRUNCATE TABLE %s', $this->tableName));
+    }
+
+}
