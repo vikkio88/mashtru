127a2400dcb866315b2a06cdec8b45399debc7af
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2017-02-08 22:28:26 +0000

    implementing JobRunner logic

--- src/Models/JobRunner.php
+++ src/Models/JobRunner.php
@@ -3,10 +3,51 @@
 
 namespace Mashtru\Libs\Models;
 
+use Exception;
+
 class JobRunner
 {
+    protected $jobNamespaces = [];
+
     function __construct($config = [])
     {
+        $this->jobNamespaces = $config['namespaces'];
     }
 
+    public function run($job)
+    {
+        try {
+            $jobCommand = $this->createJob($job->className, $job->args);
+            return $jobCommand->fire();
+        } catch (Exception $exception) {
+            $this->reportError($exception->getMessage());
+        }
+    }
+
+    private function getJobClass($jobClassName)
+    {
+        foreach ($this->jobNamespaces as $jobNamespace) {
+            $fullJobClassName = $jobNamespace . $jobClassName;
+            if (class_exists($fullJobClassName)) {
+                return $fullJobClassName;
+            }
+        }
+
+        return false;
+    }
+
+    private function createJob($jobClassName, $args)
+    {
+        $jobClass = $this->getJobClass($jobClassName);
+        if (empty($jobClass)) {
+            throw new Exception(sprintf("Class %s not found", $jobClassName));
+        }
+
+        return new $jobClass($args);
+    }
+
+    private function reportError($errorMessage)
+    {
+        echo $errorMessage;
+    }
 }
