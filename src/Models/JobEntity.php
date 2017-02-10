f1974d9b2eb83816d6df47c2fe27bee0c3606b76
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2017-02-10 19:13:14 +0000

    adding install script on entity

--- src/Models/JobEntity.php
+++ src/Models/JobEntity.php
@@ -47,4 +47,41 @@ class JobEntity extends Model
             ['fire_time' => $nextFireTime]
         );
     }
+
+    public function uninstall()
+    {
+        $this->pdo->query('DROP TABLE IF EXISTS ' . $this->tableName);
+    }
+
+    public function install()
+    {
+        if (!$this->alreadyInstalled()) {
+            $this->pdo->query(
+                'CREATE TABLE ' . $this->tableName . '(
+                id SMALLINT NOT NULL AUTO_INCREMENT,
+                name VARCHAR(255),
+                class_name VARCHAR(255) DEFAULT NULL ,
+                args TEXT DEFAULT NULL ,
+                delta_minutes INT DEFAULT NULL,
+                active BOOL DEFAULT 1,
+                fire_time TIMESTAMP NULL DEFAULT NULL,
+                updated_at TIMESTAMP NOT NULL DEFAULT NOW() ON UPDATE NOW(),
+                created_at TIMESTAMP NOT NULL,
+                PRIMARY KEY(id),
+                INDEX `name` (`name`)
+            );
+            '
+            );
+        }
+    }
+
+    private function alreadyInstalled()
+    {
+        try {
+            $this->getOne(1);
+        } catch (\Exception $e) {
+            return false;
+        }
+        return true;
+    }
 }
