82daf465e8a07e09996c7bbca520ba51b0745490
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2017-02-10 21:37:06 +0000

    implemented all db tests

--- tests/JobEntityTest.php
+++ tests/JobEntityTest.php
@@ -7,6 +7,9 @@ use Mashtru\Models\JobEntity;
 
 class JobEntityTest extends PHPUnit_Framework_TestCase
 {
+    /**
+     * @var JobEntity
+     */
     private $jobEntity;
 
     public function setUp()
@@ -31,16 +34,12 @@ class JobEntityTest extends PHPUnit_Framework_TestCase
      */
     public function itCreatesANewJob()
     {
+        $testName = 'testJob';
         $this->jobEntity->create(
-            [
-                'name' => 'testJob',
-                'class_name' => 'TestJob',
-                'args' => null,
-                'delta_minutes' => 10,
-                'active' => true,
-                'fire_time' => null,
-            ]
+            $this->getDummyData($testName)
         );
+
+        $this->assertNotEmpty($this->jobEntity->getByName($testName));
     }
 
 
@@ -50,7 +49,13 @@ class JobEntityTest extends PHPUnit_Framework_TestCase
      */
     public function itCreatesAndRemoveANewJob()
     {
+        $testRemove = 'toRemove';
+        $this->jobEntity->create(
+            $this->getDummyData($testRemove)
+        );
 
+        $this->jobEntity->deleteByName($testRemove);
+        $this->assertEmpty($this->jobEntity->getByName($testRemove));
     }
 
     /**
@@ -59,7 +64,11 @@ class JobEntityTest extends PHPUnit_Framework_TestCase
      */
     public function itGetsOneJobByName()
     {
-
+        $testGet = 'toGet';
+        $this->jobEntity->create(
+            $this->getDummyData($testGet)
+        );
+        $this->assertNotEmpty($this->jobEntity->getByName($testGet));
     }
 
     /**
@@ -68,7 +77,30 @@ class JobEntityTest extends PHPUnit_Framework_TestCase
      */
     public function itGetsMultipleJobs()
     {
+        $testAll = 'testAll';
+        $elements = 5;
+        foreach (range(0, $elements) as $i) {
+            $this->jobEntity->create(
+                $this->getDummyData($testAll . $i)
+            );
+        }
 
+        $result = $this->jobEntity->getAll();
+        $this->assertNotEmpty($result);
+        $this->assertCount($elements + 1, $result);
+
+    }
+
+    private function getDummyData($testName)
+    {
+        return [
+            'name' => $testName,
+            'class_name' => 'TestJob',
+            'args' => null,
+            'delta_minutes' => 10,
+            'active' => true,
+            'fire_time' => null,
+        ];
     }
 
 }
