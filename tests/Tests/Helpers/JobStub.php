7c661e589393b2a794c94e305cf03c8d3856b0a1
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2017-02-10 21:27:28 +0000

    updated field name

--- tests/Tests/Helpers/JobStub.php
+++ tests/Tests/Helpers/JobStub.php
@@ -6,13 +6,13 @@ namespace Tests\Helpers;
 class JobStub
 {
     public $name;
-    public $className;
+    public $class_name;
     public $args;
 
     public function __construct($name, $className, $args = [])
     {
         $this->name = $name;
-        $this->className = $className;
+        $this->class_name = $className;
         $this->args = $args;
     }
 }
